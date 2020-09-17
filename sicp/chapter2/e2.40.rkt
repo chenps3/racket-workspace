a689d3a5fcb4ada7ead3e7d748a0b242dcf257ba
陈冠鸿 <wb-cgh760152@alibaba-inc.com>
2020-09-17 22:34:44 +0800

    12

--- sicp/chapter2/e2.40.rkt
+++ sicp/chapter2/e2.40.rkt
@@ -11,6 +11,13 @@
       nil
       (cons begin (enumerate-interval (+ begin 1) end))))
 
+(define (filter predicate seq)
+  (cond ((null? seq) nil)
+        ((predicate (car seq))
+         (cons (car seq)
+               (filter predicate (cdr seq))))
+        (else (filter predicate (cdr seq)))))
+
 ;素数判断
 (define (smallest-divisor n)
   (find-divisor n 2))
@@ -34,19 +41,53 @@
   (accumulate append nil (map proc seq)))
 
 (define (prime-sum? pair)
-  (prime? (+ (car pair) (cdr pair))))
+  (prime? (+ (car pair) (cadr pair))))
 
 (define (make-pair-sum pair)
-  (list (car pair) (cdr pair) (+ (car pair) (cdr pair))))
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+;给定自然数n，找出所有序对i,j ,满足i+j是素数
+(define (prime-sum-pairs n)
+  (map make-pair-sum (prime-sum-pair-x n)))
+
+(define (prime-sum-pair-x n)
+  (filter prime-sum? (all-pair-x n)))
+
+(define (all-pair-x n)
+  (flatmap (lambda (i)
+             (map (lambda (j) (list i j))
+                  (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+;子序列
+(define (permutations s)
+  (if (null? s)
+      (list nil)
+      (flatmap (lambda (x)
+                 (map (lambda (p) (cons x p))
+                      (permutations (remove x s))))
+               s)))
+
+(define (remove item seq)
+  (filter (lambda (x) (not (= x item))) seq))
+
+;test
+;(accumulate append nil
+;            (map (lambda (i)
+;                   (map (lambda (j) (list i j))
+;                        (enumerate-interval 1 (- i 1))))
+;                 (enumerate-interval 1 5)))
+;(enumerate-interval 1 10)
+;(prime-sum? (list 1 2))
+;(prime-sum-pair-x 10)
+(prime-sum-pairs 10)
 
 
+;e2.40
+(define (unique-pairs n)
+  (filter (lambda (x) (> (car x) (cadr x))) (all-pair-x n)))
 
-;
-(accumulate append nil
-            (map (lambda (i)
-                   (map (lambda (j) (list i j))
-                        (enumerate-interval 1 (- i 1))))
-                 (enumerate-interval 1 5)))
-(enumerate-interval 1 10)
-(prime-sum? (cons 1 2))
+(define (prime-sum-pairs-new n)
+  (map make-pair-sum (prime-sum-pair-x n)))
+;(unique-pairs 10)
 
